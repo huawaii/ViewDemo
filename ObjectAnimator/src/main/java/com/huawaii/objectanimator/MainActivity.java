b10e2ba4e0962768350c4b77958316a98cbcedf4
huawaii <huawaii@163.com>
2016-05-10 00:26:14 +0800

    Add Value Animator

--- ObjectAnimator/src/main/java/com/huawaii/objectanimator/MainActivity.java
+++ ObjectAnimator/src/main/java/com/huawaii/objectanimator/MainActivity.java
@@ -2,6 +2,7 @@ package com.huawaii.objectanimator;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
@@ -9,9 +10,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+//Animator -> ValueAnimator -> ObjectAnimator
 public class MainActivity extends Activity {
 
     private ImageView iv;
@@ -90,18 +94,52 @@ public class MainActivity extends Activity {
         oa4.setRepeatCount(1);
         oa4.setRepeatMode(ValueAnimator.REVERSE);
 
-        //设置挨个飞
-//        set.playSequentially(oa1, oa2, oa3, oa4);
-        //设置一起飞
-        set.playTogether(oa1, oa2, oa3, oa4);
+//        PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("translationX", 10, 70, 20, 100);
+//        PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("translationY", 10, 70, 20, 100);
+//        PropertyValuesHolder p3 = PropertyValuesHolder.ofFloat("scaleX", 1, 1.6f, 1.2f, 2);
+//        PropertyValuesHolder p4 = PropertyValuesHolder.ofFloat("rotation", 0, 180, 90, 360);
+//        ObjectAnimator.ofPropertyValuesHolder(iv, p1, p2, p3, p4).setDuration(2000).start();
+// 多动画时效率更高
+
+        //set.playSequentially(oa1, oa2, oa3, oa4); //设置挨个飞
+        set.playTogether(oa1, oa2, oa3, oa4); //设置一起飞
         set.start();
     }
 
     public void xml(View v) {
         Animator at = AnimatorInflater.loadAnimator(this, R.animator.objanimator);
+        at.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                Toast.makeText(MainActivity.this, "xml Object Animator", Toast.LENGTH_SHORT).show();
+            }
+        });
+        at.setInterpolator(new LinearInterpolator());
         //设置作用于哪个组件
         at.setTarget(iv);
         at.start();
+    }
+
+    public void valueAnimatorDemo(final View v) {
+        final Button button = (Button) v;
+
+//        ValueAnimator valueAnimator = ValueAnimator.ofObject(new TypeEvaluator<PointF>() {
+//            @Override
+//            public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
+//                return null;
+//            }
+//        });   //返回值更加灵活
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+        valueAnimator.setDuration(5000);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();
+                button.setText("" + value);
+            }
+        });
+        valueAnimator.start();
     }
 
 }
