3525f841d3cc36c477a8b8f75dc11367600164dd
huawaii <huawaii@163.com>
2016-05-09 22:04:43 +0800

    Add UI_template

---
+++ UI_Template/README.md
@@ -0,0 +1,19 @@
+# UI_Template
+> 来源于慕课网，[Android UI模板设计](http://www.imooc.com/learn/247)
+
+####- 设计需要的属性
+1. 分析需要的自定义属性
+2. 在res/values/attrs.xml定义声明
+
+####- 创建需要的view
+1. 在layout.xml文件中进行使用
+2. 在View的构造方法中进行获取
+
+####- 引用创建的view
+
+
+
+----------
+
+    本App的重点在于：自定义View属性，自定义ViewGroup组件
+详细内容可参看：[Android 深入理解Android中的自定义属性](http://blog.csdn.net/lmj623565791/article/details/45022631/)
---
+++ UI_Template/src/main/java/com/sloop/topbar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sloop.topbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.sloop.view.TopBar;
+import com.sloop.view.TopBar.TopBarClickListener;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TopBar topBar = (TopBar) findViewById(R.id.topbar);
+/*        topBar.setLeftIsVisible(false);
+        topBar.setRightIsVisible(false);*/
+        topBar.setOnTopBarClickListener(new TopBarClickListener() {
+
+            @Override
+            public void rightclick() {
+                Toast.makeText(MainActivity.this, "Right Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void leftclick() {
+                Toast.makeText(MainActivity.this, "Left Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
