440ec98c71f60c8068a92e97dc89425dd99dda37
李宗恒 <lizongheng@meizu.com>
2017-11-27 11:34:13 +0800

    modify xfermode

--- app_ViewDraw/src/main/java/com/huawaii/viewDraw/MainActivity.java
+++ app_ViewDraw/src/main/java/com/huawaii/viewDraw/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gotoRoundImageActivity(null);
+        //gotoRoundImageActivity(null);
+        gotoXfermodeActivity(null);
         //gotoCanvasActivity(null);
     }
 
